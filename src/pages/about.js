12d6c67d3bb784021cd64b4fba521ad30a1a3a60
Carmen <carmen-m.98@gmx.de>
2025-01-05 17:22:04 +0100

    create Seo component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import Seo from "../components/seo"
 
 const AboutPage = () => {
   return (
@@ -11,7 +12,7 @@ const AboutPage = () => {
 
 export const Head = () => (
     <>
-        <title>About me</title>
+        <Seo title="About me" />
         <meta name="description" content="description" />
     </>
 )
